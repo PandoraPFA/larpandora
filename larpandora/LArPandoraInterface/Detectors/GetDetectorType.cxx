2656f1483db942c4e4a1b95fff44161a70613f91
Dom Brailsford <dominicbrailsford@gmail.com>
2023-02-08 05:53:44 -0600

    Logic to count the number of drift directions, with a sanity check

--- larpandora/LArPandoraInterface/Detectors/GetDetectorType.cxx
+++ larpandora/LArPandoraInterface/Detectors/GetDetectorType.cxx
@@ -8,6 +8,7 @@
 
 #include "larpandora/LArPandoraInterface/Detectors/GetDetectorType.h"
 #include "larpandora/LArPandoraInterface/Detectors/DUNEFarDetVDThreeView.h"
+#include "larpandora/LArPandoraInterface/Detectors/DUNEFarDetVDThreeView30DegDriftY.h"
 #include "larpandora/LArPandoraInterface/Detectors/ICARUS.h"
 #include "larpandora/LArPandoraInterface/Detectors/LArPandoraDetectorType.h"
 #include "larpandora/LArPandoraInterface/Detectors/ProtoDUNEDualPhase.h"
@@ -23,6 +24,29 @@ namespace lar_pandora {
   LArPandoraDetectorType* detector_functions::GetDetectorType()
   {
     art::ServiceHandle<geo::Geometry const> geo;
+
+    std::set<short int> driftDirectionSet;
+    for (geo::CryostatGeo const& cryo: geo->Iterate<geo::CryostatGeo>()){
+        for (geo::TPCGeo const& TPC: cryo.IterateTPCs()) {
+            (void)driftDirectionSet.insert(TPC.DetectDriftDirection());
+        }
+    }
+    const int driftDirectionCount( (driftDirectionSet.count(1) || driftDirectionSet.count(-1)) +
+                                   (driftDirectionSet.count(2) || driftDirectionSet.count(-2)) +
+                                   (driftDirectionSet.count(3) || driftDirectionSet.count(-3)) +
+                                   driftDirectionSet.count(0) );
+
+    if (driftDirectionCount > 1)
+        throw cet::exception("LArPandora") << "LArPandoraDetectorType::GetDetectorType -- unable to "
+                                              "determine drift direction due to erroneous count.  "
+                                              "Present drift directions: \n"
+                                              "-- +x: " <<  driftDirectionSet.count(1)  << "\n"
+                                              "-- -x: " <<  driftDirectionSet.count(-1) << "\n"
+                                              "-- +y: " <<  driftDirectionSet.count(2)  << "\n"
+                                              "-- -y: " <<  driftDirectionSet.count(-2) << "\n"
+                                              "-- +z: " <<  driftDirectionSet.count(3)  << "\n"
+                                              "-- -z: " <<  driftDirectionSet.count(-3) << "\n"
+                                              "-- ??: " <<  driftDirectionSet.count(0)  << "\n";
 
     const unsigned int nPlanes(geo->MaxPlanes());
     std::set<geo::_plane_proj> planeSet;
