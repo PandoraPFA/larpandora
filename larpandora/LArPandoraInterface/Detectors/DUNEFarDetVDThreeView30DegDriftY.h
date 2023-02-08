143ae4c47882855fee1523ed313c907ea9be388e
Dom Brailsford <dominicbrailsford@gmail.com>
2023-02-08 07:54:28 -0600

    Implement rotations for the DriftY VD 30 deg geometry

--- larpandora/LArPandoraInterface/Detectors/DUNEFarDetVDThreeView30DegDriftY.h
+++ larpandora/LArPandoraInterface/Detectors/DUNEFarDetVDThreeView30DegDriftY.h
@@ -17,6 +17,41 @@ namespace lar_pandora {
      */
   class DUNEFarDetVDThreeView30DegDriftY : public VintageLArTPCThreeView {
   public:
+      DUNEFarDetVDThreeView30DegDriftY();
+
+      geo::Point_t RotateToDriftX(const geo::Point_t& globalPoint) const override;
+
+      geo::Point_t UndoRotateToDriftX(const geo::Point_t& localPoint) const override;
+
+  private:
+      geo::Rotation_t m_DriftXRotation;        ///< the rotation to make the drift axis the x-axis
+      geo::Rotation_t m_InverseDriftXRotation; ///< the rotation that undoes the drift==x rotation
   };
+
+  //------------------------------------------------------------------------------------------------------------------------------------------
+
+  DUNEFarDetVDThreeView30DegDriftY::DUNEFarDetVDThreeView30DegDriftY() :
+      VintageLArTPCThreeView(),
+      m_DriftXRotation (0., -1., 0.,
+                        1.,  0., 0.,
+                        0.,  0., 1.),
+      m_InverseDriftXRotation ( 0.,  1., 0.,
+                               -1.,  0., 0.,
+                                0.,  0., 1.) {}
+
+
+  //------------------------------------------------------------------------------------------------------------------------------------------
+
+  geo::Point_t DUNEFarDetVDThreeView30DegDriftY::RotateToDriftX(const geo::Point_t& globalPoint) const
+  {
+      return m_DriftXRotation*globalPoint;
+  }
+
+  //------------------------------------------------------------------------------------------------------------------------------------------
+
+  geo::Point_t DUNEFarDetVDThreeView30DegDriftY::UndoRotateToDriftX(const geo::Point_t& localPoint) const
+  {
+      return m_InverseDriftXRotation*localPoint;
+  }
 
 } // namespace lar_pandora
