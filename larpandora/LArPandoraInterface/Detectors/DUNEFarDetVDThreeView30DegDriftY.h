5069f9bfa3e2173e2e0df508f5ccbc3fe69249a1
Dom Brailsford <dominicbrailsford@gmail.com>
2023-02-08 04:54:59 -0600

    VDFD 30 deg skeleton classh

---
+++ larpandora/LArPandoraInterface/Detectors/DUNEFarDetVDThreeView30DegDriftY.h
@@ -0,0 +1,22 @@
+/**
+ *  @file   larpandora/LArPandoraInterface/Detectors/DUNEFarDetVDThreeView30DegDriftY.h
+ *
+ *  @brief  Detector interface for DUNE's vertical drift, 3 view far detector
+ *
+ *  $Log: $
+ */
+
+#include "larpandora/LArPandoraInterface/Detectors/VintageLArTPCThreeView.h"
+
+#include "larcore/Geometry/Geometry.h"
+
+namespace lar_pandora {
+
+  /**
+     *  @brief  Detector interface DUNE's vertical drift 30 degree view far detector with Y as the drift
+     */
+  class DUNEFarDetVDThreeView30DegDriftY : public VintageLArTPCThreeView {
+  public:
+  };
+
+} // namespace lar_pandora
